d1f9e2ecca93267e8e9d05ba1603a5cc93f4e44d
Henry Seong Wook Ha <sha16@illinois.edu>
2018-06-06 15:12:35 -0500

    added 3d plotting function and module export.

--- js/plot.js
+++ js/plot.js
@@ -2,10 +2,10 @@ const Plotly = require('plotly.js');
 
 function plot(data, colorCode, type){
   if(data[0].length == 2){
-    plot2D(data, colorCode, type);
+    plot2D(data, colorCode, 'lines');
   }
   else {
-    plot3D(data, colorCode, type);
+    plot3D(data, colorCode, 'surface');
   }
 }
 
@@ -16,12 +16,41 @@ function plot2D(data, colorCode, type){
     x.push(d[0]);
     y.push(d[1]);
   });
-  Plotly.plot('plot-wrapper',[{
+
+  var input = [{
     x: x, y: y, type: type, marker: {
       color: colorCode
     },
     line: {
       color: colorCode
     }
-  }]);
+  }];
+
+  Plotly.plot('plot-wrapper',input);
 }
+
+function plot3D(data, colorCode, type) {
+  var x = [];
+  var y = [];
+  var z = [];
+  data.forEach(function(d){
+    x.push(d[0]);
+    y.push(d[1]);
+    z.push(d[2]);
+  });
+
+  var input = [
+    {
+      opacity: 0.8,
+      
+      type: type,
+      x: x,
+      y: y,
+      z: z
+    }
+  ];
+
+  Plotly.newPlot('plot-wrapper',input);
+}
+
+module.exports.plot = plot(data, colorCode, type);
