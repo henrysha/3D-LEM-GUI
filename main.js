60d7921371e5a7ecbdd248b0ac2c1062cf939597
Jusung Han <jasicat37@gmail.com>
2018-06-19 16:05:24 -0500

    changed the way to make menu

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ const url = require('url');
 const path = require('path');
 const {app, BrowserWindow, Menu, ipcMain, dialog} = electron;
 
-const myPlot = require('./js/plot');
-const parse = require('./js/parsecsv');
-const {plot} = myPlot;
-const {parseCsv} = parse;
+
+
+
 // Set ENV (Uncomment for the final product)
 //process.env.NODE_ENV = 'production';
 
@@ -29,123 +28,38 @@ app.on('ready', function(){
   });
   
   
-  // Build menu from template
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  // Insert Menu
-  Menu.setApplicationMenu(mainMenu);
+  // // Build menu from template
+  // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // // Insert Menu
+  // Menu.setApplicationMenu(mainMenu);
+  require('./js/mainmenu');
 });
 
-/* Handle create add window
-function createAddWindow(){
-  // Create new window
-  addWindow = new BrowserWindow({
-    width: 300,
-    height: 200,
-    title:'Add Shopping List Item'
-  });
-  // Load html into window
-  addWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'addWindow.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-  // Garbage collection Handle
-  addWindow.on('closed', function(){
-    addWindow = null;
-  })
-}
 
-// Catch item:add
-ipcMain.on('item:add',function(e, item){
-  mainWindow.webContents.send('item:add', item);
-  addWindow.close();
-});
-*/
-// function to upload csv files to the GUI
-function addCsvFiles(filepath){
-  
-  var data = parseCsv(filepath, function(result){
-    plot(result.data, 'black', 'lines');
-  });
-}
 
-// Create menu template
-const mainMenuTemplate = [
-  {
-    label:'File',
-    submenu:[
-      {
-        label: 'Open File',
-        click(){
-          dialog.showOpenDialog({
-            filters: [
-              { name: 'CSV', extensions: ['csv']},
-              { name: 'All Files', extensions: ['*']}
-            ]
-          }, selectedFiles => addCsvFiles(selectedFiles));
-        }
-      },
-      {
-        label: 'Open Folder',
-        click(){
 
-        }
-      },
-      {
-        label: 'Save',
-        click(){
-
-        }
-      },
-      {
-        label:'Exit',
-        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
-          app.quit();
-        }
-      }
-    ]
-  },
-  {
-    label:'Edit',
-    submenu:[
-      {
-        label: 'Add Item',
-        click(){
-          //createAddWindow();
-        }
-      },
-      {
-        label: 'Clear Items',
-        click(){
-          mainWindow.webContents.send('item:clear');
-        }
-      },
-
-    ]
-  },
-];
 
 // If mac, add empty object to menu
-if(process.platform == 'darwin'){
-  mainMenuTemplate.unshift({});
-}
+// already added in mainmenu.js => could manipulate later
+// if(process.platform == 'darwin'){
+//   mainMenuTemplate.unshift({});
+// }
 
 // Add developer tools item if not in prod
-if(process.env.NODE_ENV !== 'production'){
-  mainMenuTemplate.push({
-    label: 'Developer Tools',
-    submenu:[
-      {
-        label: 'Toggle DevTools',
-        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-        click(item, focusedWindow){
-          focusedWindow.toggleDevTools();
-        }
-      },
-      {
-        role: 'reload'
-      }
-    ]
-  });
-}
+// if(process.env.NODE_ENV !== 'production'){
+//   mainMenuTemplate.push({
+//     label: 'Developer Tools',
+//     submenu:[
+//       {
+//         label: 'Toggle DevTools',
+//         accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+//         click(item, focusedWindow){
+//           focusedWindow.toggleDevTools();
+//         }
+//       },
+//       {
+//         role: 'reload'
+//       }
+//     ]
+//   });
+// }
